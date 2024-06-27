793c6ff737ebb1b1e78734597d1a41362939211a
FluffyFoxTail <fluffyfoxtail988@gmail.com>
2024-06-27 23:41:10 +0300

    refactor vector struct

--- boids/vec2d.go
+++ boids/vec2d.go
@@ -2,40 +2,48 @@ package boids
 
 import "math"
 
-type Vector2D struct {
+type Vector struct {
 	x float64
 	y float64
 }
 
-func (v1 Vector2D) Add(v2 Vector2D) Vector2D {
-	return Vector2D{v1.x + v2.x, v1.y + v2.y}
+func NewVector2D(x, y float64) Vector {
+	return Vector{x, y}
 }
 
-func (v1 Vector2D) Subtract(v2 Vector2D) Vector2D {
-	return Vector2D{v1.x - v2.x, v1.y - v2.y}
+func (v Vector) X() float64 { return v.x }
+
+func (v Vector) Y() float64 { return v.y }
+
+func (v Vector) Add(v2 Vector) Vector {
+	return Vector{v.x + v2.x, v.y + v2.y}
 }
 
-func (v1 Vector2D) Multiply(v2 Vector2D) Vector2D {
-	return Vector2D{v1.x * v2.x, v1.y * v2.y}
+func (v Vector) Subtract(v2 Vector) Vector {
+	return Vector{v.x - v2.x, v.y - v2.y}
 }
 
-func (v1 Vector2D) AddV(d float64) Vector2D {
-	return Vector2D{v1.x + d, v1.y + d}
+func (v Vector) Multiply(v2 Vector) Vector {
+	return Vector{v.x * v2.x, v.y * v2.y}
 }
 
-func (v1 Vector2D) MultiplyV(d float64) Vector2D {
-	return Vector2D{v1.x * d, v1.y * d}
+func (v Vector) AddV(d float64) Vector {
+	return Vector{v.x + d, v.y + d}
 }
 
-func (v1 Vector2D) DivisionV(d float64) Vector2D {
-	return Vector2D{v1.x / d, v1.y / d}
+func (v Vector) MultiplyV(d float64) Vector {
+	return Vector{v.x * d, v.y * d}
 }
 
-func (v1 Vector2D) limit(lower, upper float64) Vector2D {
-	return Vector2D{math.Min(math.Max(v1.x, lower), upper),
-		math.Min(math.Max(v1.y, lower), upper)}
+func (v Vector) DivisionV(d float64) Vector {
+	return Vector{v.x / d, v.y / d}
 }
 
-func (v1 Vector2D) Distance(v2 Vector2D) float64 {
-	return math.Sqrt(math.Pow(v1.x-v2.x, 2) + math.Pow(v1.y-v2.y, 2))
+func (v Vector) limit(lower, upper float64) Vector {
+	return Vector{math.Min(math.Max(v.x, lower), upper),
+		math.Min(math.Max(v.y, lower), upper)}
+}
+
+func (v Vector) Distance(v2 Vector) float64 {
+	return math.Sqrt(math.Pow(v.x-v2.x, 2) + math.Pow(v.y-v2.y, 2))
 }
